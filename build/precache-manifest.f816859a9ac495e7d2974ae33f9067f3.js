fe48d8e92bd04b95eec8dd996890af707ef5d378
Pink Moonlight <kate.cherie25@gmail.com>
2020-04-24 14:59:15 -0700

    updated background color and build files

---
+++ build/precache-manifest.f816859a9ac495e7d2974ae33f9067f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c30f7a7edc98dd74d72e",
+    "url": "/static/css/main.95be23df.chunk.css"
+  },
+  {
+    "revision": "c30f7a7edc98dd74d72e",
+    "url": "/static/js/main.831662b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45a7acfdace2e315d2d9",
+    "url": "/static/js/2.ee49d4bf.chunk.js"
+  },
+  {
+    "revision": "b448cbc668dfc3517d51da62e482e44f",
+    "url": "/index.html"
+  }
+];
