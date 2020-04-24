124cd85b675c30548964b293c08150471a0f8206
Pink Moonlight <kate.cherie25@gmail.com>
2020-04-24 15:31:09 -0700

    tried to deploy app

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c871dd31cf36f0105c5abfb01cd5698b.js"
+  "/Name-Tag-Generator/precache-manifest.a427b9bdab5861f632d64ca2ca827031.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Name-Tag-Generator/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
