2afaf2ff81d6ae5b5f1acd62f7ba4da88e58a565
Pink Moonlight <kate.cherie25@gmail.com>
2020-04-24 17:05:39 -0700

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
-
-import './styles.css';
-
-var rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
